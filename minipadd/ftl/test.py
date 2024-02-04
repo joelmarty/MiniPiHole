e8e5d830eeff872d01731a6f4531c353e596f36e
Joël Marty <134835+joelmarty@users.noreply.github.com>
2024-02-04 17:39:09 +0100

    Refactor: remove pihole setup config

--- minipadd/ftl/test.py
+++
@@ -1,9 +0,0 @@
-from unittest import TestCase
-
-from minipadd import ftl
-
-
-class Test(TestCase):
-    def test_get_stats(self):
-        stats = ftl.get_stats('192.168.50.97', '96c3780287c58bd0867c8cd9b2d60c387ea070c4df3f87d2d3e3c770d3baab0b')
-        print(stats)
